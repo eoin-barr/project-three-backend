bf669c2ec0277396c204f831a7bbea50554047cd
eoin-barr <eoin.barr@ucdconnect.ie>
2021-09-23 11:09:11 +0100

    Secret updated

--- config/environment.js
+++ config/environment.js
@@ -3,4 +3,4 @@ dotenv.config()
 
 export const dbURI = process.env.DB_URI || 'mongodb://localhost/airstudio-db'
 export const port = process.env.PORT || 4000
-export const secret = 'dD18aS02c5'
+export const secret = process.env.SECRET || 'dD18aS02c5'
